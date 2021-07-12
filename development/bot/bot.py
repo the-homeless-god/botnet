2888cf9ce6d3a5a6ee4804f36f5f2252fc6aac40
Uzhastin-Nikita <fosfor20@mail.ru>
2021-07-12 15:04:10 +0300

    feat: adding message processing

--- development/bot/bot.py
+++ development/bot/bot.py
@@ -6,27 +6,45 @@ class Bot:
         return self.message
     
     def dialog(self, message):
-        self.message = message.lower()
+        self.message = message
 
 
         """data for dialog: recognizing meaning from messages"""
         
-        greeting_message = ['привет', 'добрый утро', 'добрый день', 'добрый вечер', 'здравствуй', 'здравствуйте', 'приветствую']
+        greeting_message = ['привет', 'доброеутро', 'добрыйдень', 'добрыйвечер', 'здравствуй', 'здравствуйте', 'приветствую', 'прив', 'здрасти', 'ктоты', 'тыкто']
 
-        mood_message = ['как дела', 'как оно', 'как настроение', 'как успехи', 'всё нормально']
+        mood_message = ['как дела', 'каконо', 'какнастроение', 'какуспехи', 'всёнормально']
 
         farewell_message = ['пока', 'прощай', 'до свидания', 'удачи', 'успехов']
 
+        isuct_message = ['химтех', 'хим', 'ивановскийгосударственныйхимико-технологическийуниверситет', 'исукт', 'исакт', 'игтху']
+
         """dialog"""
+
+        # greeting
         if message in greeting_message:
             return 'Привет, я бот Name'
         
+        # mood
         if message in mood_message:
             return 'Всё плохо, меня заперли в этой коробке и заставляют разговаривать с людьми'
         
+        # farewell
         if message in farewell_message:
             return 'Приходи ещё'
+        
+        # ISUCT message: different phrases and slogans
+        if message in isuct_message:
+            return 'Диплом химтеха - залог успеха!'
 
         return 'Я вас не понимаю'
     
-        
+    def context_save(self, message):
+        self.message = message
+
+        context = []
+
+        """save context message"""
+        context.append(message)
+
+    
--- development/bot/server.py
+++ development/bot/server.py
@@ -1,14 +1,30 @@
 from bot import Bot
 from adapter import Adapter
 
+import string
+
 def main():
     bot = Bot()
     
     while(True):
         message = input()
 
-        message = message.lower()
+        # preparing a message
 
+        message = message.lower()
+        
+        # removing punctuation marks
+        for item in string.punctuation:
+            if item in message:
+                message = message.replace(item, '')
+        
+        # delete spaces
+        message = message.strip()
+
+        # we save the context so that we can track what the user said later
+        bot.context_save(message)
+        
+        # dialog
         print(bot.dialog(message))
 
 main()
