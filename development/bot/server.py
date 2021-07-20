d84ea58f29942af86eb04685b15a0bad4f8c3966
Nikita <71666299+Uzhastin-Nikita@users.noreply.github.com>
2021-07-20 11:12:07 +0300

    BS - Bot (#30)
    * feat: adding message processing

--- development/bot/server.py
+++ development/bot/server.py
@@ -1,6 +1,8 @@
 from bot import Bot
 from adapter import Adapter
 
+import string
+
 def main():
   
     bot = Bot()
@@ -8,8 +10,21 @@ def main():
     while(True):
         message = input()
 
+        # preparing a message
         message = message.lower()
+        
+        # removing punctuation marks
+        for item in string.punctuation:
+            if item in message:
+                message = message.replace(item, '')
+        
+        # delete spaces
+        message = message.strip()
 
+        # we save the context so that we can track what the user said later
+        bot.context_save(message)
+        
+        # dialog
         print(bot.dialog(message))
 
 main()
