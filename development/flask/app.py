cb4ce0a4e9ecd29d28563d160670c5ceda266695
Uzhastin-Nikita <fosfor20@mail.ru>
2021-07-25 21:51:11 +0300

    Addition flask app

--- development/flask/app.py
+++ development/flask/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+
+from server import Server
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+@app.route("/")
+def home():    
+    return render_template("index.html") 
+
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(Server(userText))
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ development/flask/server.py
@@ -0,0 +1,12 @@
+from bot import Bot
+
+def Server(message):
+    bot = Bot()
+
+    while(True):
+        # we save the context so that we can track what the user said later
+        bot.context_save(message)
+        
+        # dialog
+        return bot.dialog(message)
+    
