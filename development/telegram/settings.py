a9d41e2542be986cb01ac369d8cb1d3729ab3484
Uzhastin-Nikita <fosfor20@mail.ru>
2021-07-25 15:20:20 +0300

    Update settings.py

--- development/telegram/settings.py
+++ development/telegram/settings.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-if os.path.exists('telegram/template.env'):
-    load_dotenv(".env")
+if os.path.exists("template.env"):
+    load_dotenv("template.env")
+
     TELEGRAM_TOKEN = os.getenv("TOKEN")
