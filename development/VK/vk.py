7fc100772ca064ac011edd92dbd6f36dfc6717c9
Uzhastin-Nikita <fosfor20@mail.ru>
2021-08-05 16:22:04 +0300

    BS - VKontakte: addition comments

--- development/VK/vk.py
+++ development/VK/vk.py
@@ -9,11 +9,13 @@ import random
 def write_message(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.getrandbits(64)})
 
-
+# We log in as a community
 vk = vk_api.VkApi(token=settings.VK_TOKEN)
 
+# Working with messages
 longpoll = VkLongPoll(vk)
 
+# main cycle
 for event in longpoll.listen():
 
     if event.type == VkEventType.MESSAGE_NEW:
