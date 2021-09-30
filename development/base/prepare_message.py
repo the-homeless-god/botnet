4d349bc564aff9fb0b8a80fbb7dadcb541af52b4
Uzhastin-Nikita <fosfor20@mail.ru>
2021-09-30 20:35:32 +0300

    fix: folder problems: duplicate code

--- development/base/prepare_message.py
+++
@@ -1,4 +0,0 @@
-def prepare_message(message):
-    message = message.replace('.', '').replace(',', '').replace('?', '').replace('!', '').replace('.', '').replace(':', '').replace(' ', '').lower()
-
-    return message
